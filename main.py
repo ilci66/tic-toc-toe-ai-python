13ab2f714b899a335df2d8106788c56062583462
unknown <akbiyik.ilker7@gmail.com>
2022-08-20 21:20:33 +0200

    computer moves basic added

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ def choose_letter():
     else:
         return False
 
+
 # TODO: randomly decide who goes first
 def who_goes_first():
     if random.randint(0, 1) == 0:
@@ -28,15 +29,16 @@ def who_goes_first():
     else:
         return 'player'
 
+
 # TODO: ask if they wanna go again, use input
 def play_again():
-    print('Wanna play again ?')
+    print('Wanna play again ? y / n')
     return input().lower().startswith('y')
 
 
 # TODO: place the letter on the board
-def place_letter():
-    pass
+def place_letter(board, letter, move):
+    board[move] = letter
 
 
 # TODO: check if the space on the board is empty
@@ -85,21 +87,54 @@ def get_player_move(board):
 
 
 # TODO: do a random move in a possible slot
-def get_random_move_from_list():
-    pass
+def get_random_move_from_list(board, moves_list):
+    moves = []
+    for i in moves_list:
+        if is_free(board, i):
+            moves.append(i)
+    if len(moves) != 0:
+        return random.choice(moves)
+    else:
+        return None
 
 
 # TODO: determine where to place the letter and return the letter
-def get_computer_move():
+def get_computer_move(board, com_letter):
+    player_letter = " "
+    if com_letter == 'X':
+        player_letter = 'O'
+    else:
+        player_letter = 'X'
+
     # TODO: check if there's a winner, return winner
+    for i in range(9):
+        copy = copy_board(board)
+        if is_free(copy, i):
+            place_letter(copy, com_letter, i)
+            if is_win(copy, com_letter):
+                return i
+
     # TODO: check if there is a corner free, return corner
+    for i in range(9):
+        copy = copy_board(board)
+        if is_free(copy, i):
+            place_letter(copy, player_letter, i)
+            if is_win(copy, player_letter):
+                return i
+
     # TODO: check if there is side free, return get_random_move_from_list
-    pass
+    move = get_random_move_from_list(board, [1, 3, 7, 9])
+    if move is not None:
+        return move
+
+    if is_free(board, 5):
+        return 5
+
+    return get_random_move_from_list(board, [2, 4, 6, 8])
 
 
 # TODO: check if the board is full
 def is_board_full(board):
-
     for i in range(9):
         if is_free(board, i):
             return False
