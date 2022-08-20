7fb42a6156467a601191f2f4955d4337676c0f03
unknown <akbiyik.ilker7@gmail.com>
2022-08-20 16:13:03 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,74 @@
+import random
+
+
+# TODO: draw the board in the console
+def draw_board():
+    pass
+
+
+# TODO: player chooses their letter, use input
+def choose_letter():
+    pass
+
+
+# TODO: randomly decide who goes first
+def who_goes_first():
+    pass
+
+
+# TODO: ask if they wanna go again, use input
+def play_again():
+    pass
+
+
+# TODO: place the letter on the board
+def place_letter():
+    pass
+
+
+# TODO: check if the space on the board is empty
+def is_free():
+    pass
+
+
+# TODO: check if there is a winner
+def is_win():
+    pass
+
+
+# TODO: copy the board and return the copy
+def copy_board():
+    pass
+
+
+# TODO: get the player move and register its legitimacy
+def get_player_move():
+    pass
+
+
+# TODO: do a random move in a possible slot
+def get_random_move_from_list():
+    pass
+
+
+# TODO: determine where to place the letter and return the letter
+def get_computer_move():
+    # TODO: check if there's a winner, return winner
+    # TODO: check if there is a corner free, return corner
+    # TODO: check if there is side free, return get_random_move_from_list
+    pass
+
+
+# TODO: check if the board is full
+def is_board_full():
+    pass
+
+
+# TODO: start the game
+def start_game():
+    pass
+
+
+if __name__ == '__main__':
+    print('Works !!!')
+
